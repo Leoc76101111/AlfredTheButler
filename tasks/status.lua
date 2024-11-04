34f750a0ea9c1b80a130c2e436db28d3f4efe7db
Leoc <a@a.a>
2024-11-05 00:07:18 +1100

    added status and tracker

---
+++ tasks/status.lua
@@ -0,0 +1,33 @@
+local plugin_label = "alfred_the_butler"
+local utils = require "core.utils"
+local settings = require "core.settings"
+local tracker = require "core.tracker"
+local gui = require "gui"
+
+
+local status_task = {
+    name = "Status",
+    status = "Idle"
+}
+
+function status_task.shouldExecute()
+    if not utils.player_in_zone("Scos_Cerrigar") then
+        return true
+    end
+    return false
+end
+
+function status_task.Execute()
+    local local_player = get_local_player()
+    if not local_player then
+        return
+    end
+    local item_count = tracker.salvage_count + tracker.sell_count
+    if item_count >= tracker.inventory_limit then
+        status_task.status = "Waiting to be in Cerrigar"
+    else
+        status_task.status = "Idle"
+    end
+end
+
+return status_task
